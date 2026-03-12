0336593361739abb7f1a3905a16430b0c60915b3
blakejosh <214008066+blakejosh54@users.noreply.github.com>
2026-03-12 09:52:44 +0200

    logic, theme, board, icons, setup

--- src/components/GameCell/GameCell.jsx
+++ src/components/GameCell/GameCell.jsx
@@ -2,19 +2,40 @@ import React, { useContext } from "react";
 import { CellStyle } from "./GameCell.styled";
 import { GameContext } from "../../contexts/GameContext";
 import { checkForWinner } from "../../utils/GameUtils";
+import IconO from "../../assets/icon-o.svg?react";
+import IconX from "../../assets/icon-x.svg?react";
+import XIconOutline from "../../assets/icon-x-outline.svg?react";
+import OIconOutline from "../../assets/icon-o-outline.svg?react";
 
 function GameCell({ cellItem, index }) {
-  const { updateBoard,game } = useContext(GameContext);
+  const { updateBoard, game } = useContext(GameContext);
 
   const cellClickHandler = () => {
-    updateBoard(index)
+    updateBoard(index);
     const result = checkForWinner(game.board);
-    // if(result) {
-      
-    // }
-
   };
-  return <CellStyle onClick={cellClickHandler}>{cellItem}</CellStyle>;
+
+  if (cellItem === "x") {
+    return (
+      <CellStyle>
+        <IconX className="markedItem"/>
+      </CellStyle>
+    );
+  } else if (cellItem === "o") {
+    return (
+      <CellStyle>
+        <IconO className="markedItem"/>
+      </CellStyle>
+    );
+  }
+
+  return (
+    <CellStyle onClick={cellClickHandler}>
+      {game.turn === "x" ? 
+      <XIconOutline className="outlineIconX"/> :
+      <OIconOutline className="outlineIconO"/>}
+    </CellStyle>
+  );
 }
 
 export default GameCell;
--- src/components/GameCell/GameCell.styled.jsx
+++ src/components/GameCell/GameCell.styled.jsx
@@ -5,9 +5,37 @@ export const CellStyle = styled.button`
   color: ${(props) => props.theme.colors.primary};
   font-size: 3rem;
   border: none;
-  border-radius: 10px;
+  border-radius: 2.5rem;
   box-shadow: 5px 10px ${(props) => props.theme.colors.gray};
   width: 10rem;
   height: 10rem;
   cursor: pointer;
+  padding: 3rem;
+
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+
+  .markedItem {
+    path {
+      fill: ${(props) => props.theme.colors.primary};
+    }
+  }
+
+  .outlineIconX path,
+  .outlineIconO path {
+    fill: transparent;
+    stroke: transparent;
+    stroke-width: 0;
+  }
+
+  &:hover .outlineIconX path {
+    stroke: ${(props) => props.theme.colors.primary};
+    stroke-width: 12;
+  }
+
+  &:hover .outlineIconO path {
+    fill: ${(props) => props.theme.colors.primary};
+  }
 `;
